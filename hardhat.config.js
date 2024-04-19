665dfcc6aa0ddea5d4345f543120e4398364fd0d
Aaron Li <lichenlc@gmail.com>
2024-04-19 22:51:18 +0800

    clean up the code

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/qDl0-U3YgkFXpYqH7IK4ti_szxsAAN_N",
+        url: "https://eth-mainnet.g.alchemy.com/v2/<YOU_API_KEY_HERE>",
         blockNumber: 19362010,
       },
     },
